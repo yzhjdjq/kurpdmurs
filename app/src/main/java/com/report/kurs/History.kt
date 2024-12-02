006683017b25611c454b4eadbf5058fbe7f73221
yzhjdjq <yzhjdjq@mail.ru>
2024-12-02 18:48:25 +0300

    Модуль database внедрен в историю игр
    В классе игр частично внедрено взаимодействие с настройками и модулем database

--- app/src/main/java/com/report/kurs/History.kt
+++ app/src/main/java/com/report/kurs/History.kt
@@ -1,26 +1,54 @@
 package com.report.kurs
 
+import android.content.Context
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material.icons.filled.Warning
+import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.State
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.room.Room.databaseBuilder
+import com.report.kurs.database.Database
+import com.report.kurs.database.DatabaseName
+import com.report.kurs.database.ResultGameModel
 import com.report.kurs.ui.theme.KursTheme
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import kotlinx.serialization.Serializable
 
 @Serializable
@@ -28,8 +56,19 @@ object HistoryData
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun History(onBackClicked: () -> Unit ) {
+fun History(onBackClicked: () -> Unit) {
     val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior(rememberTopAppBarState())
+    val context = LocalContext.current
+    val coroutine = rememberCoroutineScope()
+    var history = databaseBuilder(
+        context,
+        Database::class.java,
+        DatabaseName.Get()
+    ).build().GetResultGameDao().GetAllResults()
+        .collectAsStateWithLifecycle(listOf(ResultGameModel()))
+    val historyIsEmpty = remember{ mutableStateOf( history.value.size == 0 || (history.value.size == 1 && history.value[0].id == null))}
+    var showClearHistoryDialog = remember { mutableStateOf( false ) }
+    historyIsEmpty.value = history.value.size == 0 || (history.value.size == 1 && history.value[0].id == null)
 
     Scaffold(
         modifier = Modifier.fillMaxSize(),
@@ -54,6 +93,19 @@ fun History(onBackClicked: () -> Unit ) {
                         )
                     }
                 },
+                actions = {
+                    IconButton(
+                        onClick = {
+                            showClearHistoryDialog.value = true
+                        },
+                        enabled = !historyIsEmpty.value
+                    ) {
+                        Icon(
+                            imageVector = Icons.Filled.Delete,
+                            contentDescription = "Очистить историю"
+                        )
+                    }
+                },
                 scrollBehavior = scrollBehavior
             )
         },
@@ -63,10 +115,137 @@ fun History(onBackClicked: () -> Unit ) {
                 .fillMaxSize()
                 .padding(innerPadding)
         ) {
-            Column(Modifier.align(Alignment.Center)) {
+            if (showClearHistoryDialog.value)
+                AlertDialogClearHistory(
+                    onDismiss = {
+                        showClearHistoryDialog.value = false
+                    },
+                    onConfirmation = {
+                        coroutine.launch {
+                            ClearHistory(context, history)
+                        }
+                        showClearHistoryDialog.value = false
+                        historyIsEmpty.value = true
+                    }
+                )
+            if (historyIsEmpty.value)
+                Column(modifier = Modifier.align(Alignment.Center)) {
+                    Text(
+                        text = "Чтобы увидеть историю,\nсыграйте хотя бы один раз",
+                        textAlign = TextAlign.Center,
+                        fontSize = 16.sp,
+                        fontWeight = FontWeight.Bold,
+                        lineHeight = 32.sp
+                    )
+                }
+            else
+                LazyColumn(modifier = Modifier) {
+                    items(history.value) {
+                        HistoryRecord(
+                            result = it.result,
+                            date = it.date,
+                            sizeOfArena = it.sizeOfArena,
+                            countOfMines = it.countOfMines,
+                            modifier = Modifier
+                        )
+                    }
+                }
+        }
+    }
+}
 
+@Composable
+fun HistoryRecord(
+    result: String,
+    date: String,
+    sizeOfArena: Int,
+    countOfMines: Int,
+    modifier: Modifier = Modifier
+) {
+    Column(modifier = modifier.padding(5.dp)) {
+        Text(
+            text = result,
+            fontSize = 20.sp,
+            fontWeight = FontWeight.ExtraBold,
+            modifier = modifier.padding(top = 10.dp, start = 10.dp, bottom = 0.dp)
+        )
+
+        Text(
+            text = "Дата: " + date,
+            fontSize = 14.sp,
+            fontWeight = FontWeight.Bold,
+            modifier = modifier.padding(top = 5.dp, start = 30.dp)
+        )
+        Row(modifier) {
+
+            Text(
+                text = "Размер поля: " + sizeOfArena.toString() + "Х" + sizeOfArena.toString(),
+                fontSize = 14.sp,
+                fontWeight = FontWeight.Bold,
+                modifier = modifier.padding(top = 5.dp, start = 30.dp)
+            )
+
+            Text(
+                text = "Количество мин на поле: " + countOfMines.toString(),
+                fontSize = 14.sp,
+                fontWeight = FontWeight.Bold,
+                modifier = modifier.padding(top = 5.dp, start = 30.dp)
+            )
+        }
+
+        HorizontalDivider(
+            thickness = 3.dp,
+            modifier = modifier
+                .padding(horizontal = 5.dp)
+                .padding(top = 15.dp)
+        )
+    }
+}
+
+@Composable
+fun AlertDialogClearHistory(
+    onDismiss: () -> Unit,
+    onConfirmation: () -> Unit
+) {
+    AlertDialog(
+        icon = {
+            Icon(Icons.Default.Warning, contentDescription = "Потенциально опасная операция")
+        },
+        title = {
+            Text(text = "Очистить историю игр?")
+        },
+        text = {
+            Text(text = "После очистки истории игр не будет возможности ее восстановить.")
+        },
+        onDismissRequest = { onDismiss() },
+        confirmButton = {
+            TextButton(
+                onClick = {
+                    onConfirmation()
+                }
+            ) {
+                Text("Удалить")
+            }
+        },
+        dismissButton = {
+            TextButton(onClick = {
+                onDismiss()
+            }) {
+                Text("Отменить")
             }
         }
+    )
+}
+
+suspend fun ClearHistory(context: Context, history: State<List<ResultGameModel>>) {
+    val resDao = databaseBuilder(
+        context,
+        Database::class.java,
+        DatabaseName.Get()
+    ).build().GetResultGameDao()
+    withContext(Dispatchers.IO) {
+        for (record in history.value)
+            resDao.DelereResult(record)
     }
 }
 
@@ -74,6 +253,6 @@ fun History(onBackClicked: () -> Unit ) {
 @Composable
 fun HistoryPreview() {
     KursTheme {
-        History{}
+        History {}
     }
 }
--- app/src/main/java/com/report/kurs/game/Game.kt
+++ app/src/main/java/com/report/kurs/game/Game.kt
@@ -1,10 +1,9 @@
 package com.report.kurs.game
 
 import android.content.Context
-import android.content.Context.MODE_PRIVATE
 import android.graphics.Color.rgb
+import android.icu.text.SimpleDateFormat
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -34,6 +33,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -43,7 +43,17 @@ import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.room.Room
+import com.report.kurs.database.Database
+import com.report.kurs.database.DatabaseName
+import com.report.kurs.database.ResultGameModel
+import com.report.kurs.settings.Settings
 import com.report.kurs.ui.theme.KursTheme
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import java.util.Date
+import java.util.Locale
 
 class Game : ComponentActivity() {
     @OptIn(ExperimentalMaterial3Api::class)
@@ -78,7 +88,11 @@ class Game : ComponentActivity() {
                         )
                     }
                 ) { innerPadding ->
-                    Box(modifier = Modifier.fillMaxSize().padding(innerPadding)) {
+                    Box(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(innerPadding)
+                    ) {
                         Column(Modifier.align(Alignment.Center)) {
                             GamePage()
                         }
@@ -91,15 +105,15 @@ class Game : ComponentActivity() {
 
 @Composable
 fun GamePage() {
-    val gridDimension = 5
-    val totalMines = 5
+    val context = LocalContext.current
+    val coroutineScope = rememberCoroutineScope()
+
+    val gridDimension = Settings.GetSizeOfArena(context, 6)
+    val totalMines = Settings.GetCountOfMines(context, 6)
     var grid by remember { mutableStateOf(createMinefield(gridDimension, totalMines)) }
     var firstMove by remember { mutableStateOf(true) }
     var statusMessage by remember { mutableStateOf("Начни играть!") }
-    var flaggingMode by remember { mutableStateOf(false) }
-    val context = LocalContext.current
-    val pref = context.getSharedPreferences("MineSweeperSettings", MODE_PRIVATE)
-    Log.d( "MyLog", pref.getInt("sizeOfArena", 0).toString() )
+    var flaggingMode by remember { mutableStateOf(Settings.GetFlaggingMode(context, false)) }
 
     Column(
         modifier = Modifier
@@ -140,14 +154,21 @@ fun GamePage() {
                     if (grid[x][y].isMine) {
                         statusMessage = "О нет, ты попал на мину!"
                         grid = exposeAllMines(grid)
+                        coroutineScope.launch {
+                            SaveResultGame(context, "Проиграл", gridDimension, totalMines)
+                        }
                     } else {
                         grid = handleFirstMove(grid, x, y)
                         firstMove = false
+                        statusMessage = "Так держать!"
                     }
                 } else {
                     if (grid[x][y].isMine) {
                         statusMessage = "О нет, ты попал на мину!"
                         grid = exposeAllMines(grid)
+                        coroutineScope.launch {
+                            SaveResultGame(context, "Проиграл", gridDimension, totalMines)
+                        }
                     } else {
                         grid = grid.mapIndexed { rowIndex, rowList ->
                             rowList.mapIndexed { colIndex, tile ->
@@ -158,6 +179,14 @@ fun GamePage() {
                                 }
                             }
                         }
+                        var winFlag = true
+                        for( row in grid )
+                            if( row.contains(Title(isRevealed = false))) {
+                                winFlag = false
+                                break
+                            }
+                        if( winFlag )
+                            statusMessage = "Поздравляю, вы победили!"
                     }
                 }
             }
@@ -174,6 +203,9 @@ fun GamePage() {
 
         OutlinedButton(
             onClick = {
+                coroutineScope.launch {
+                    SaveResultGame(context, "Не закончена", gridDimension, totalMines)
+                }
                 grid = createMinefield(gridDimension, totalMines)
                 firstMove = true
                 statusMessage = "Начни играть!"
@@ -184,6 +216,29 @@ fun GamePage() {
         ) {
             Text(text = "Начать заново", color = Color.Black)
         }
+    }
+}
+
+suspend fun SaveResultGame(context: Context, result: String, sizeOfArena: Int, countOfMines: Int) {
+    withContext(Dispatchers.IO) {
+        val db = Room.databaseBuilder(
+            context,
+            Database::class.java,
+            DatabaseName.Get()
+        ).build()
+        val resultDao = db.GetResultGameDao()
+        resultDao.AddResult(
+            ResultGameModel(
+                id = null,
+                date = SimpleDateFormat(
+                    "YYYY-MM-dd HH:mm:ss",
+                    Locale.getDefault()
+                ).format(Date()),
+                result = result,
+                sizeOfArena = sizeOfArena,
+                countOfMines = countOfMines
+            )
+        )
     }
 }
 
