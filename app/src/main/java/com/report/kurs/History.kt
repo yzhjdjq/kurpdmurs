28f9528233af53fe06e433666daa0b0598e5251b
yzhjdjq <yzhjdjq@mail.ru>
2024-12-04 18:52:06 +0300

    Добавлена полоса прокрутки в историю игр

--- app/src/main/java/com/report/kurs/History.kt
+++ app/src/main/java/com/report/kurs/History.kt
@@ -1,13 +1,17 @@
 package com.report.kurs
 
 import android.content.Context
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.filled.Delete
@@ -30,14 +34,18 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawWithContent
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
@@ -60,15 +68,19 @@ fun History(onBackClicked: () -> Unit) {
     val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior(rememberTopAppBarState())
     val context = LocalContext.current
     val coroutine = rememberCoroutineScope()
+    var scrollBarState = rememberLazyListState()
+
     var history = databaseBuilder(
         context,
         Database::class.java,
         DatabaseName.Get()
     ).build().GetResultGameDao().GetAllResults()
         .collectAsStateWithLifecycle(listOf(ResultGameModel()))
-    val historyIsEmpty = remember{ mutableStateOf( history.value.size == 0 || (history.value.size == 1 && history.value[0].id == null))}
-    var showClearHistoryDialog = remember { mutableStateOf( false ) }
-    historyIsEmpty.value = history.value.size == 0 || (history.value.size == 1 && history.value[0].id == null)
+    val historyIsEmpty =
+        remember { mutableStateOf(history.value.size == 0 || (history.value.size == 1 && history.value[0].id == null)) }
+    var showClearHistoryDialog = remember { mutableStateOf(false) }
+    historyIsEmpty.value =
+        history.value.size == 0 || (history.value.size == 1 && history.value[0].id == null)
 
     Scaffold(
         modifier = Modifier.fillMaxSize(),
@@ -139,7 +151,12 @@ fun History(onBackClicked: () -> Unit) {
                     )
                 }
             else
-                LazyColumn(modifier = Modifier) {
+                LazyColumn(
+                    state = scrollBarState,
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .VerticalScrollbar(scrollBarState)
+                ) {
                     items(history.value) {
                         HistoryRecord(
                             result = it.result,
@@ -150,6 +167,40 @@ fun History(onBackClicked: () -> Unit) {
                         )
                     }
                 }
+        }
+    }
+}
+
+@Composable
+fun Modifier.VerticalScrollbar(
+    state: LazyListState,
+    width: Dp = 6.dp
+): Modifier {
+    val targetAlpha = if (state.isScrollInProgress) 1f else 0f
+    val duration = if (state.isScrollInProgress) 150 else 1000
+
+    val alpha by animateFloatAsState(
+        targetValue = targetAlpha,
+        animationSpec = tween(durationMillis = duration)
+    )
+
+    return drawWithContent {
+        drawContent()
+
+        val firstVisibleElementIndex = state.layoutInfo.visibleItemsInfo.firstOrNull()?.index
+        val needDrawScrollbar = state.isScrollInProgress || alpha > 0.0f
+
+        if (needDrawScrollbar && firstVisibleElementIndex != null) {
+            val elementHeight = this.size.height / state.layoutInfo.totalItemsCount
+            val scrollbarOffsetY = firstVisibleElementIndex * elementHeight
+            val scrollbarHeight = state.layoutInfo.visibleItemsInfo.size * elementHeight
+
+            drawRect(
+                color = Color.Gray,
+                topLeft = Offset(this.size.width - width.toPx(), scrollbarOffsetY),
+                size = Size(width.toPx(), scrollbarHeight),
+                alpha = alpha
+            )
         }
     }
 }
